97a70c43b1689b295280aba4d9c89fd213f531a6
Bryan <bryan.curchod@gmail.com>
2019-01-10 00:32:11 +0100

    remove nestjs mongo

--- src/databaseManager.ts
+++ src/databaseManager.ts
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 export class dbManager {
 
-    dbURL = 'mongodb://localhost:27017/demo';
+    dbURL = 'mongodb://localhost:27017/brycur-TWEB-TE2';
     client;
     constructor() {
         MongoClient.connect(this.dbURL).then((client) => { this.client = client });
--- src/schemas/user.schemas.ts
+++
@@ -1,6 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const UsersSchemas = new mongoose.Schema({
-    user: String,
-    password: String,
-});
